fd6ccefbedb4e16d472a54574dac30db8819c698
Sebastian Garcia <sag44402@uga.edu>
2023-09-16 13:17:56 -0400

    routes implemented

--- Frontend/cinema-ebooking/src/App.js
+++ Frontend/cinema-ebooking/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import CustomerHomePage from './components/CustomerHomePage';
 
 function App() {
   return (
-    <div>
-      <CustomerHomePage />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<CustomerHomePage/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
