279ed624ebc4bbdb3563a92162d81a1b379ea414
Sebastian Garcia <sag44402@uga.edu>
2023-09-16 12:15:35 -0400

    start of customer homepage navbar

---
+++ Frontend/cinema-ebooking/src/components/CustomerHomePage.js
@@ -0,0 +1,14 @@
+import './CustomerHomePage.css'
+
+function CustomerHomePage() {
+    return (
+        <>
+            <div className='navbar'>
+                <b1 className='titlelogo'>Cinema E-Booking A8</b1>
+                <button className='login-button'>Login</button>
+            </div>
+        </>
+    )
+}
+
+export default CustomerHomePage;
