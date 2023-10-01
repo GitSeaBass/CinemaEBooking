415164b0ad333094fba56a5d0d07fda5bef5eafb
Connor Bell <ronnoclleb@gmail.com>
2023-10-01 16:05:59 -0400

    fix existing movie trailers

--- Frontend/cinema-ebooking/src/App.js
+++ Frontend/cinema-ebooking/src/App.js
@@ -12,7 +12,7 @@ function App() {
         poster: 'https://i.ebayimg.com/images/g/1EcAAOSwa-9klig5/s-l1600.jpg',
         rating: 7.1,
         status: 'soon',
-        trailer: 'https://www.youtube.com/embed/vZ734NWnAHA'
+        trailer: 'https://www.youtube.com/embed/pBk4NYhWNMM'
     },
     {
         title: 'Star Wars',
@@ -26,28 +26,28 @@ function App() {
         poster: 'https://media.comicbook.com/2018/07/aqamn-vert-tsr-dom-2764x4096-r01-master-1122913.jpeg',
         rating: 8.2,
         status: 'now',
-        trailer: 'https://www.youtube.com/embed/vZ734NWnAHA'
+        trailer: 'https://www.youtube.com/embed/WDkg3h8PCVU'
     },
     {
         title: 'Knives Out',
         poster: 'https://m.media-amazon.com/images/I/51oWGDskkhL.jpg',
         rating: 6.8,
         status: 'now',
-        trailer: 'https://www.youtube.com/embed/vZ734NWnAHA'
+        trailer: 'https://www.youtube.com/embed/qGqiHJTsRkQ'
     },
     {
         title: 'Spongebob Movie',
         poster: 'https://m.media-amazon.com/images/M/MV5BZjM5YjI0NmQtOTk4OS00NTNiLThkNzQtNTZlNGE4Y2VmNmU3XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg',
         rating: 9.9,
         status: 'soon',
-        trailer: 'https://www.youtube.com/embed/vZ734NWnAHA'
+        trailer: 'https://www.youtube.com/embed/a2cowVH03Xo'
     },
     {
         title: 'Morbius',
         poster: 'https://www.themoviedb.org/t/p/original/6JjfSchsU6daXk2AKX8EEBjO3Fm.jpg',
         rating: 4.7,
         status: 'soon',
-        trailer: 'https://www.youtube.com/embed/vZ734NWnAHA'
+        trailer: 'https://www.youtube.com/embed/oZ6iiRrz1SY'
     },
     {
         title: 'Spongebob Movie',
