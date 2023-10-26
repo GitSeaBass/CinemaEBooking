113fab4e822a58dfe58c7665f3e91e81f09188c6
Matt Sand <matt.sand119@gmail.com>
2023-10-26 14:39:07 -0400

    Fixed runtime errors from improper repository set up.

--- Backend/src/main/java/com/system/backend/Customer.java
+++ Backend/src/main/java/com/system/backend/Customer.java
@@ -1,18 +1,66 @@
 package com.system.backend;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 @Entity
-public class Customer extends User {
+public class Customer {
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
     private String dateOfBirth;
     public static enum Status {ACTIVE, INACTIVE, SUSPENDED};
     private Status status;
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "address_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Address address;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public String getDateOfBirth() {
         return dateOfBirth;
