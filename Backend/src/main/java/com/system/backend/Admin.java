75ee2bee37541f13ec12c800e96ae18f1e0db6fb
Matt Sand <matt.sand119@gmail.com>
2023-10-30 10:30:38 -0400

    Adding getters and setters to Admin

--- Backend/src/main/java/com/system/backend/Admin.java
+++ Backend/src/main/java/com/system/backend/Admin.java
@@ -12,4 +12,43 @@ public class Admin {
     private String email;
     private String password;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
