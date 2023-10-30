598e32d6266938901dee67b2f02a33e2a3f1d202
Matt Sand <matt.sand119@gmail.com>
2023-10-30 10:29:39 -0400

    Adding getters and setters to Address

--- Backend/src/main/java/com/system/backend/Address.java
+++ Backend/src/main/java/com/system/backend/Address.java
@@ -13,4 +13,44 @@ public class Address {
     private String city;
     private String state;
     private Integer zipcode;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Integer getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(Integer zipcode) {
+        this.zipcode = zipcode;
+    }
 }
