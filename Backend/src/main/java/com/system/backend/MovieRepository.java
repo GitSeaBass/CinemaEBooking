7b2bef743495c69a2100ecdc890a66d438245e20
Sebastian Garcia <sag44402@uga.edu>
2023-11-19 22:30:13 -0500

    changed Integer to String in MovieRepository

--- Backend/src/main/java/com/system/backend/MovieRepository.java
+++ Backend/src/main/java/com/system/backend/MovieRepository.java
@@ -3,6 +3,6 @@ package com.system.backend;
 import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
-public interface MovieRepository extends CrudRepository<Movies, Integer> {
+public interface MovieRepository extends CrudRepository<Movies, String> {
     List<Movies> findByTitle(String title);
 }
