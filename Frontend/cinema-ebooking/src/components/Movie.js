cdda3aaaf99a80a703c876d86957d8bdea7b2e6e
Matthew Weiss <mew34254@uga.edu>
2023-12-04 02:21:46 -0500

    added the rest of movie information and some blind style

--- Frontend/cinema-ebooking/src/components/Movie.js
+++ Frontend/cinema-ebooking/src/components/Movie.js
@@ -17,12 +17,18 @@ function Movie() {
                 <iframe src={location.state.movie.trailer_url} title={location.state.movie.title} className="Movie-trailer" />
                 <div className='Movie-information-container'>
                     <div className='Movie-left'>
-                        <img src={location.state.movie.poster_url} className='Movie-movie-poster' />
+                        <p>{location.state.movie.mpaa_rating}</p>
+                        <p>{location.state.movie.category}</p>
                         <button className='Movie-get-tickets-button' onClick={grabTickets}>Get Tickets</button>
+                        <img src={location.state.movie.poster_url} className='Movie-poster'></img>
                     </div>
                     <div className='Movie-right'>
                         <p>{location.state.movie.synopsis}</p>
+                        <p className='Movie-people'>Cast: {location.state.movie.cast}</p>
+                        <p className='Movie-people'>Directed by: {location.state.movie.director}</p>
+                        <p className='Movie-people'>Produced by: {location.state.movie.producer}</p>
                     </div>
+                    <p>{location.state.movie.reviews}</p>
                 </div>
             </div>
         </>
